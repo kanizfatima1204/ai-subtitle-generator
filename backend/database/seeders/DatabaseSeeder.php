7f7bed74a7307df3a9e7c1ce8a87029604926703
Kaniz Fatima <kfatima1204@gmail.com>
2026-09-17 20:14:02 +0600

    fix: use updateOrCreate to always reset admin password on deploy

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::firstOrCreate(
+        User::updateOrCreate(
             ['email' => 'admin@agency.com'],
             [
                 'name' => 'Admin',
